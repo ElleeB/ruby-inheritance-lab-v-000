0b5ef2baf34db16ed1ee3c37598728ef0f956b6e
Laura Trager <laura.trager@gmail.com>
2017-12-01 18:39:01 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,11 +3,12 @@ require_relative "./user.rb"
 
 class Teacher < User
 
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   attr_accessor :first_name, :last_name
 
   def teach
-    KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+    KNOWLEDGE.each{|string| puts string}
   end
 
 end
