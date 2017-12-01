1a2ea087ffc144e8660ecb5c5b89cdd4a7f9722e
Laura Trager <laura.trager@gmail.com>
2017-12-01 18:43:36 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,12 +5,8 @@ class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  attr_accessor :first_name, :last_name
-
   def teach
-    count = 0
-    puts KNOWLEDGE[count]
-    count += 1
+    puts KNOWLEDGE[.sample
   end
 
 end
